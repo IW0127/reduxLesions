ef033b40f1e2a50d389e611b5d7c371684538c05
hardikIdetix <hardik.dholariya@elookinto.org>
2022-07-18 16:07:08 +0530

    01_lesson

---
+++ 01_lesson/src/App.js
@@ -0,0 +1,11 @@
+import Counter from "./features/counter/Counter";
+
+function App() {
+  return (
+    <div className="container my-2">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
