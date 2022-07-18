94d965bb1b5a649462bc27b7bc01e661a708b2cf
hardikIdetix <hardik.dholariya@elookinto.org>
2022-07-18 16:08:00 +0530

    03_lesson

---
+++ 03_lesson/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./assets/css/App.css";
+import AddPostForm from "./features/posts/AddPostForm";
+import PostsList from "./features/posts/PostsList";
+
+function App() {
+  return (
+    <main className="App">
+      <AddPostForm />
+      <PostsList />
+    </main>
+  );
+}
+export default App;
