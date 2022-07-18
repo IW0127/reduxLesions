6d971d433b72bc6a5a8e4f2164d2a832d5e234fb
hardikIdetix <hardik.dholariya@elookinto.org>
2022-07-18 16:07:35 +0530

    02_lesson

---
+++ 02_lesson/run.bat
@@ -0,0 +1,3 @@
+code .
+yarn start
+echo "Test!"
---
+++ 02_lesson/src/features/post/PostList.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectAllPosts } from "./postSlice";
+import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
+import ReactionButton from "./ReactionButton";
+
+function PostList() {
+  const posts = useSelector(selectAllPosts);
+
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderedPosts = orderedPosts.map((post) => (
+    <article key={post.id}>
+      <h3>{post.title}</h3>
+      <p> {post.content.substring(0, 100)} </p>
+      <p className="postCredit">
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+      <ReactionButton post={post} />
+    </article>
+  ));
+
+  return (
+    <section>
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  );
+}
+
+export default PostList;
