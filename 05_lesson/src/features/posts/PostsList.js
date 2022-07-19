7b9da3999e20a745b55b656f8400318fba66462e
hardikIdetix <hardik.dholariya@elookinto.org>
2022-07-19 16:33:50 +0530

    05_lesson

---
+++ 05_lesson/src/features/posts/PostsList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { getPostsError, getPostsStatus, selectPostIds } from './postsSlice';
+import PostsExcerpt from './PostsExcerpt';
+
+function PostsList() {
+  const orderedPostsIds = useSelector(selectPostIds);
+  const postStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
+
+  let content;
+  if (postStatus === 'loading') {
+    content = <p>"Loading...."</p>;
+  } else if (postStatus === 'succeeded') {
+    content = orderedPostsIds.map((postId) => (
+      <PostsExcerpt key={postId} postId={postId} />
+    ));
+  } else if (postStatus === 'failed') {
+    content = <p>{error}</p>;
+  }
+
+  return <section>{content}</section>;
+}
+
+export default PostsList;
