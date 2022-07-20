3abb23f958f6cb29ad111fd888ffd1aa9d90454e
hardikIdentix <hardik.dholariya@elookinto.org>
2022-07-20 15:16:23 +0530

    sdf

--- 06_lesson/src/features/todos/TodoList.js
+++ 06_lesson/src/features/todos/TodoList.js
@@ -1,4 +1,9 @@
-import { useGetTodosQuery } from '../api/apiSlice';
+import {
+  useGetTodosQuery,
+  useAddTodoMutation,
+  useUpdateTodoMutation,
+  useDeleteTodoMutation,
+} from '../api/apiSlice';
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons';
@@ -14,9 +19,14 @@ const TodoList = () => {
     isError,
     error,
   } = useGetTodosQuery();
+
+  const [addTodo] = useAddTodoMutation();
+  const [updateTodo] = useUpdateTodoMutation();
+  const [deleteTodo] = useDeleteTodoMutation();
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    //addTodo
+    addTodo({ userId: 1, title: newTodo, completed: false });
     setNewTodo('');
   };
 
@@ -39,7 +49,32 @@ const TodoList = () => {
   );
 
   let content;
-  // Define conditional content
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  } else if (isSuccess) {
+    content = todos.map((todo) => {
+      return (
+        <article key={todo.id}>
+          <div className='todo'>
+            <input
+              type='checkbox'
+              checked={todo.completed}
+              id={todo.id}
+              onChange={() =>
+                updateTodo({ ...todo, completed: !todo.completed })
+              }
+            />
+            <label htmlFor={todo.id}>{todo.title} </label>
+          </div>
+          <button className='trash' onClick={() => deleteTodo({ id: todo.id })}>
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
+        </article>
+      );
+    });
+  } else if (isError) {
+    content = <p>{error}</p>;
+  }
 
   return (
     <main>
